bc2d1af1cdb92c36d4549eb6dfd3d180c012fe1a
Supratims <supratims@gmail.com>
2017-06-09 15:44:24 +0100

    Initial commit - webhose test

---
+++ README.md
@@ -0,0 +1,9 @@
+# webhose-api
+Simple application to fetch ecommerce data from webhose
+
+## See documentation
+https://webhose.io/product-data-api
+
+## Requirements
+* nodejs
+* obtain a webhose key and save it in a file api_key
---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * Simple script to fetch webhose content
+ */
+const https = require('https');
+const fs = require('fs');
+const querystring = require('querystring');
+
+/*
+ * Make sure you have a api_key file with the key from guardian 
+ */
+const options = {
+  key: fs.readFileSync('./api_key').toString().trim(),
+};
+
+function urlBuilder(url, params){
+	return url+'?token='+options.key+'&'+querystring.stringify(params);
+}
+const base_url = 'https://webhose.io/productFilter';
+const product_url = urlBuilder(base_url, {q: 'name:iphone 6s'});
+
+console.log("fetching "+product_url);
+fetch(product_url);
+
+function fetch(url, callback){
+    https.get(url, (res) => {
+      res.on('data', (d) => {
+        var json = JSON.parse(d.toString());
+	var products = json.products;
+	products.forEach(function(prod){		
+	    console.log(prod.price);
+	});
+      });
+    
+    }).on('error', (e) => {
+      console.error(e);
+    });
+}
